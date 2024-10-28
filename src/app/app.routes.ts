db558b442f0e8200ac22cb33a33a20179eddece3
Chelsea Gomes <chelsealizygomes11@gmail.com>
2024-10-28 15:16:00 +0530

    created Login and Layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { MasterComponent } from './Components/master/master.component';
 import { EmployeeComponent } from './Components/employee/employee.component';
 import { ClientComponent } from './Components/client/client.component';
 import { ClientProjectComponent } from './Components/client-project/client-project.component';
+import { LoginComponent } from './Components/login/login.component';
+import { LayoutComponent } from './Components/layout/layout.component';
+import path from 'path';
 
 export const routes: Routes = [
     {
@@ -11,19 +14,28 @@ export const routes: Routes = [
         pathMatch: 'full'
     },
     {
-        path:'master',
-        component: MasterComponent
+        path:'login',
+        component: LoginComponent
     },
     {
-        path:'employee',
-        component: EmployeeComponent
-    },
-    {
-        path:'client',
-        component: ClientComponent
-    },
-    {
-        path:'client-project',
-        component: ClientProjectComponent
-    },
-];
+        path:'',
+        component: LayoutComponent,
+        children: [
+            {
+            path:'master',
+        component: MasterComponent},
+        {
+            path:'employee',
+            component: EmployeeComponent
+        },
+        {
+            path:'client',
+            component: ClientComponent
+        },
+        {
+            path:'client-project',
+            component: ClientProjectComponent
+        }
+     ]
+       
+    }]
